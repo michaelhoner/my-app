74397406e5bb24d0db42de5fcf3dc622187c766b
Michael <43703002+michaelhoner@users.noreply.github.com>
2019-05-07 17:37:09 +0200

    git

---
+++ build/precache-manifest.6fb703597005d8879f933fbce5efd672.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "d50f3efb75f7f1a7e8f0",
+    "url": "/my-app/static/js/main.e8744a71.chunk.js"
+  },
+  {
+    "revision": "2adde99a68ed9e9b0577",
+    "url": "/my-app/static/js/2.196b3dee.chunk.js"
+  },
+  {
+    "revision": "d50f3efb75f7f1a7e8f0",
+    "url": "/my-app/static/css/main.2d15ffdb.chunk.css"
+  },
+  {
+    "revision": "9374cf9bfb652971389c7115eb2f2de6",
+    "url": "/my-app/index.html"
+  }
+];
